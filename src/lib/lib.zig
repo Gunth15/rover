6419f472442bd989147a154ef687fdbcf82f245f
Gunth15 <cjw041@email.latech.edu>
2026-03-06 12:36:45 -0600

    Pico integrated

--- src/lib/lib.zig
+++ src/lib/lib.zig
@@ -1,7 +1,9 @@
 pub const ring_cluster = @import("util/ring_cluster.zig");
 pub const BlockingQueue = @import("util/blocking_queue.zig").BlockingQueue;
-pub const IOThread = @import("iothread/iothread.zig");
+pub const Io = @import("iothread/io/io.zig");
+pub const HttpParser = @import("httpparser/httpparser.zig");
 test {
     _ = @import("util/ring_cluster.zig");
     _ = @import("iothread/iothread.zig");
+    _ = @import("httpparser/httpparser.zig");
 }
--- src/lib/util/ring_cluster.zig
+++ src/lib/util/ring_cluster.zig
@@ -3,50 +3,126 @@ const atomic = std.atomic;
 
 //WARNING: Undefined Behavior if tqil goes beyond 2^64 -1
 //TODO: add crash when that happens or handle gracefully
-pub fn Ring(T: type, comptime Size: comptime_int) type {
-    comptime {
-        if (!std.math.isPowerOfTwo(Size)) @compileError("Given ring buffer size is not a power of two");
-    }
+
+pub fn ThreadRing(comptime T: type) type {
     return struct {
         const Self = @This();
-        pub const MASK = Size - 1;
-        const Cursor = struct { v: atomic.Value(usize) = .init(0), _padding: [atomic.cache_line - @sizeOf(atomic.Value(usize))]u8 = undefined };
+
+        const Cursor = struct {
+            v: atomic.Value(usize) = .init(0),
+            _padding: [atomic.cache_line - @sizeOf(atomic.Value(usize))]u8 = undefined,
+        };
+
         head: Cursor align(atomic.cache_line) = .{},
         tail: Cursor align(atomic.cache_line) = .{},
-        //Fit on four cache lines
-        data: [Size]T = undefined,
+        data: []T = undefined,
 
-        pub fn enqueue(q: *Self, data: T) error{RingFull}!void {
+        pub fn init(alloc: std.mem.Allocator, capacity: usize) !Self {
+            if (!std.math.isPowerOfTwo(capacity))
+                return error.NotPowerOfTwo;
+
+            return .{
+                .data = try alloc.alloc(T, capacity),
+            };
+        }
+
+        pub fn enqueue(q: *Self, value: T) !void {
             const tail = q.tail.v.load(.acquire);
-            const index = tail & MASK;
-            //Can only load use n-1 elemnts in the array for sake of simplicity
-            //should be negligable
-            if (tail - q.head.v.load(.acquire) >= q.data.len) return error.RingFull;
-            q.data[index] = data;
+
+            if (tail - q.head.v.load(.acquire) >= q.data.len)
+                return error.RingFull;
+
+            const index = tail & (q.data.len - 1);
+            q.data[index] = value;
+
             q.tail.v.store(tail + 1, .release);
         }
+
         pub fn dequeue(q: *Self) ?T {
             const head = q.head.v.load(.acquire);
-            if (head == q.tail.v.load(.acquire)) return null;
-            //Can only load use n-1 elemnts in the array for sake of simplicity
-            //should be negligable
-            const val = q.data[head & MASK];
+
+            if (head == q.tail.v.load(.acquire))
+                return null;
+
+            const value = q.data[head & (q.data.len - 1)];
+
             q.head.v.store(head + 1, .release);
-            return val;
+            return value;
+        }
+
+        pub fn deinit(q: *const Self, alloc: std.mem.Allocator) void {
+            alloc.free(q.data);
         }
     };
 }
 
-pub fn Cluster(Submission: type, Completion: type, RingCount: comptime_int, RingSize: comptime_int) type {
+pub fn Ring(comptime T: type) type {
+    return struct {
+        const Self = @This();
+
+        head: usize = 0,
+        tail: usize = 0,
+        data: []T = undefined,
+
+        pub fn init(alloc: std.mem.Allocator, capacity: usize) !Self {
+            if (!std.math.isPowerOfTwo(capacity))
+                return error.NotPowerOfTwo;
+
+            return .{
+                .data = try alloc.alloc(T, capacity),
+            };
+        }
+
+        pub fn enqueue(q: *Self, value: T) !void {
+            const tail = q.tail;
+
+            if (tail - q.head >= q.data.len)
+                return error.RingFull;
+
+            const index = tail & (q.data.len - 1);
+            q.data[index] = value;
+
+            q.tail += 1;
+        }
+
+        pub fn dequeue(q: *Self) ?T {
+            const head = q.head;
+
+            if (head == q.tail)
+                return null;
+
+            const value = q.data[head & (q.data.len - 1)];
+            q.head += 1;
+
+            return value;
+        }
+
+        pub fn deinit(q: *Self, alloc: std.mem.Allocator) void {
+            alloc.free(q.data);
+        }
+    };
+}
+
+pub fn Cluster(Submission: type, Completion: type) type {
     return struct {
         pub const Self = @This();
-        pub const SubmissionRing = Ring(Submission, RingSize);
-        pub const CompletionRing = Ring(Completion, RingSize * 2);
 
-        submission_pool: [RingCount]SubmissionRing = .{SubmissionRing{}} ** RingCount,
-        completion_pool: [RingCount]CompletionRing = .{CompletionRing{}} ** RingCount,
+        pub const SubmissionRing = ThreadRing(Submission);
+        pub const CompletionRing = ThreadRing(Completion);
+        submission_pool: []SubmissionRing,
+        completion_pool: []CompletionRing,
 
         //Bulk dequeue on given queue
+        pub fn init(alloc: std.mem.Allocator, ring_size: usize, ring_count: usize) error{ NotPowerOfTwo, OutOfMemory }!Self {
+            const completion_pool = try alloc.alloc(CompletionRing, ring_count);
+            const submission_pool = try alloc.alloc(SubmissionRing, ring_count);
+            for (0..completion_pool.len) |i| completion_pool[i] = try .init(alloc, ring_size);
+            for (0..submission_pool.len) |i| submission_pool[i] = try .init(alloc, ring_size);
+            return .{
+                .completion_pool = completion_pool,
+                .submission_pool = submission_pool,
+            };
+        }
         pub fn pullSubmissions(c: *Self, id: usize, submission: []Submission) []Submission {
             if (submission.len == 0) return submission;
             var sq = &c.submission_pool[id];
@@ -56,16 +132,23 @@ pub fn Cluster(Submission: type, Completion: type, RingCount: comptime_int, Ring
 
             const dequeueable = if (entries > submission.len) submission.len else entries;
             for (0..dequeueable) |i| {
-                submission[i] = sq.data[(head + i) & SubmissionRing.MASK];
+                submission[i] = sq.data[(head + i) & (sq.data.len - 1)];
             }
             sq.head.v.store(head + dequeueable, .release);
             return submission[0..dequeueable];
+        }
+        pub fn deinit(c: *Self, alloc: std.mem.Allocator) void {
+            for (c.completion_pool) |cq| cq.deinit(alloc);
+            for (c.submission_pool) |sq| sq.deinit(alloc);
+            alloc.free(c.completion_pool);
+            alloc.free(c.submission_pool);
         }
     };
 }
 
 test "general ring buffer test" {
-    var ring: Ring(u64, 256) = .{};
+    var ring: Ring(u64) = try .init(std.testing.allocator, 256);
+    defer ring.deinit(std.testing.allocator);
 
     for (0..256) |i| {
         const e = ring.enqueue(i);
@@ -83,7 +166,7 @@ test "multithreaded cluster test" {
     const THREADS = 5;
     const RINGSIZE = 4096;
     const COUNT = 4096;
-    const RingCluster = Cluster(u64, u64, THREADS, RINGSIZE);
+    const RingCluster = Cluster(u64, u64);
     const producercb = struct {
         var COMPLETED: [THREADS][COUNT]bool = .{.{false} ** COUNT} ** THREADS;
         fn push_numbers(id: usize, s: *RingCluster.SubmissionRing, c: *RingCluster.CompletionRing) void {
@@ -118,7 +201,8 @@ test "multithreaded cluster test" {
         }
     };
 
-    var cluster: RingCluster = .{};
+    var cluster: RingCluster = try .init(std.testing.allocator, RINGSIZE, THREADS);
+    defer cluster.deinit(std.testing.allocator);
 
     var thread: [THREADS]std.Thread = undefined;
     for (0..THREADS) |i| {
