14bf934d27747ce909521af65e2d6055909cf214
Gunth15 <cjw041@email.latech.edu>
2026-05-02 18:12:47 -0500

    Expanding future capabilities

--- src/Future.zig
+++ src/Future.zig
@@ -16,3 +16,13 @@ pub inline fn wake(f: *Future, runtime: *Runtime) State {
 pub inline fn cancel(f: *Future, runtime: *Runtime) void {
     return f.vtable.cancel(f, runtime, f.conn, f.ctxt);
 }
+///run one after another. If one fails, the next is not ran
+pub fn seq() Future {}
+///runs all at the same time and returns all results
+pub fn group() Future {}
+///If one fails, they all fail and are cancelled
+pub fn failGroup() Future {}
+///First one to finish is returned without failure, the others are cancelled(doe snot gurantee the others will be completed however)
+pub fn select() Future {}
+///First one to finish is returned, even if it fails, the others are cancelled
+pub fn failSelect() Future {}
